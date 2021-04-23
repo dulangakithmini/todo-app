dde97066efb7e4f99defdf4530c95a7a454e0743
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-23 22:03:20 +0530

    Create ToDo app

---
+++ ToDo/Data/ItemsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDo.Data
+{
+    public class ItemsContext : DbContext
+    {
+        public ItemsContext(DbContextOptions<ItemsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Models.Item> Items { get; set; }
+    }
+}
---
+++ ToDo/Pages/Items/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDo.Models;
+
+namespace ToDo.Pages_Items
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ToDo.Data.ItemsContext _context;
+
+        public IndexModel(ToDo.Data.ItemsContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Item> Item { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Item = await _context.Items.ToListAsync();
+        }
+    }
+}
