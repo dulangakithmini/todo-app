1cd2cacbfc5c047de04f027f139bd2fdbfb18182
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-04-24 12:15:53 +0530

    Put create ToDo option to Index page

--- ToDo/Pages/Items/Index.cshtml.cs
+++ ToDo/Pages/Items/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ToDo.Models;
@@ -20,6 +21,22 @@ namespace ToDo.Pages_Items
         public async Task OnGetAsync()
         {
             Item = await _context.Items.ToListAsync();
+        }
+
+        [BindProperty] public Item Item1 { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Items.Add(Item1);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
